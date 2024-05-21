8eda26d2c5145252187f341cb1852e1281141e50
muhammet075 <muhammet075@icloud.com>
2024-05-22 01:35:21 +0200

    push

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html lang="nl">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WXC5CN7M0Q"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-R48LSETZ9C"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-WXC5CN7M0Q');`
+          gtag('config', 'G-R48LSETZ9C');`
         }}
       />
       </Head>
